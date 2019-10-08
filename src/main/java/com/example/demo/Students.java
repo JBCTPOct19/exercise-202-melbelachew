4b4d5e4d9a20a8e3326a88832a1819459f506a4b
Mel Belachew <melbelachew@gmail.com>
2019-10-08 10:26:20 -0400

    first commit

---
+++ src/main/java/com/example/demo/Students.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class Students {
+    private String name;
+    private int age;
+    private int height;
+    private long phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
